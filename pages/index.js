6148d105ca635f17c08a5aac4b87ee280ac58638
zavier <zavier.lowe@gmail.com>
2021-10-18 11:34:52 -0400

    Getting data for products from shopify

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navigation from './Navigation';
 import LandingPage from './LandingPage';
 import Footer from './Footer';
+import Head from "next/head";
+import Image from "next/image";
 
-export default function Home() {
+import { client } from "../utils/shopify";
+import Link from 'next/link'
+import React,{useEffect , useState} from "react";
+
+
+
+export default function Home({products}) {
+
+  console.log({products});
+  
+  
+  
   return (
     <div className={styles.container}>
+      {/* <div>
+        {products.map(product =>{
+          return(
+            <Link key={product.id} href={`product/${product.id}`}>
+            <p key={product.id}>{product.title}</p></Link>
+          )
+        })}
+      </div> */}
 
     <Navigation />
     <LandingPage />
@@ -19,4 +38,15 @@ export default function Home() {
    
 
   )
+}
+  
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const products = await client.product.fetchAll()
+  
+  
+
+  // Pass data to the page via props
+  return { props: { products: JSON.parse(JSON.stringify(products)) } }
 }
